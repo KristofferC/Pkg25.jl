67997477ca4a9857d2965e486a82879fa7d1e63a
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2016-12-19 19:27:11 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,6 @@ using Pkg25
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
-
-tic()
-Pkg25.init()
-println("Initialized in ", toc, " seconds")
-toc()
 Pkg25.add("Crayons")
 Pkg25.status()
 Pkg25.resolve()
